70e809d1afaf3cc641b591209c155dc1daeb7662
Chris <chris.renschler@gmail.com>
2019-10-27 15:58:35 +0000

    Reorganizing files in src and bin

---
+++ src/simulator.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+// cout for character printed by bitstream
+// cerr for not genuine output
+
+int main(int argc, char *argv[]) {
+
+    string binName = argv[1];
+
+    cerr << binName;
+
+    return 1;
+}
