4d7f95ecbc0fd98a5c03792158600bc9ed448182
Chris <chris.renschler@gmail.com>
2019-10-28 00:18:13 +0000

    Const variables, binary input , pc counter conditions.
    Const variables for offset and length of areas in memory; now reads a binary input character by character; layed out basic conditions for pc counter.

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <fstream>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -9,9 +10,52 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 
-    string binName = argv[1];
+    const int imem_off = 0x10000000;
+    const int imem_length = 0x1000000;
 
-    cerr << binName;
+    const int dmem_off = 0x20000000;
+    const int dmem_length = 0x4000000;
+
+    std::cerr << "Simulating binary " << argv[1] << endl;
+
+    ifstream bin_stream(argv[1], ios::binary);
+
+    //get length of binary instructions of input file
+    bin_stream.seekg(0, bin_stream.end);
+    int bin_length = bin_stream.tellg();
+    bin_stream.seekg(0, bin_stream.beg);
+
+    // temporarily store all binary instructions character by character
+    char * buffer = new char[bin_length];
+    bin_stream.read(&buffer[0], bin_length);
+
+    cerr << "Read " << bin_length << " characters" << endl;
+
+    vector<uint32_t> imem;
+    imem.resize(imem_length);
+
+    vector<uint32_t> dmem;
+    dmem.resize(dmem_length);
+
+    vector<uint32_t> reg;
+    reg.resize(32);
+
+    uint32_t pc = imem_off;
+
+    while(1) {
+        uint32_t instruction;
+        if(pc == 0) {
+            //execution finished
+            exit(0);
+        } else if(pc < imem_off) {
+
+        } else if(pc < imem_off + imem_length) {
+            instruction = imem[pc / 4]; // !!!!! why / 4
+        } else {
+            //memory exception
+            exit(-11);
+        }
+    }
 
     return 1;
 }
