df0c8e0ead121f0ef770dd2348be1a2fdf3dc3cf
Nikita Belenkov <nikitov603@gmail.com>
2019-10-30 12:29:42 +0000

    Loads the bin file into a buffer of bytes. Convertion to uint32 doesnt work as of yet

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -5,12 +5,14 @@
 
 #include "decoder.h"
 
-using namespace std;
 
 // cout for character printed by bitstream
 // cerr for not genuine output
 
-/* int main(int argc, char *argv[]) {
+void printvector(std::vector<unsigned char> v);
+std::vector<uint32_t> void convert(std::vector<unsigned char> vec);
+
+int main(int argc, char *argv[]) {
 
     const int imem_off = 0x10000000;
     const int imem_length = 0x1000000;
@@ -18,28 +20,40 @@ using namespace std;
     const int dmem_off = 0x20000000;
     const int dmem_length = 0x4000000;
 
-    std::cerr << "Simulating binary " << argv[1] << endl;
+    std::cerr << "Simulating binary " << argv[1] << std::endl;
 
-    ifstream bin_stream(argv[1], ios::binary);
+    std::ifstream bin_stream(argv[1], std::ios::binary);
+
 
     //get length of binary instructions of input file
     bin_stream.seekg(0, bin_stream.end);
     int bin_length = bin_stream.tellg();
     bin_stream.seekg(0, bin_stream.beg);
 
+    std::vector<uint32_t> imem;
+    imem.resize(imem_length); //taking all memory
+
+
     // temporarily store all binary instructions character by character
-    char * buffer = new char[bin_length];
-    bin_stream.read(&buffer[0], bin_length);
+    //char * buffer = new char[bin_length];
+    //bin_stream.read(&buffer[0], bin_length);
+    std::cout << bin_length << '\n';
+    std::vector<unsigned char> immembyte(std::istreambuf_iterator<char>(bin_stream), {});
 
-    cerr << "Read " << bin_length << " characters" << endl;
+    std::cerr << "Read " << immembyte.size() << std::endl;
+    printvector(immembyte);
 
-    vector<uint32_t> imem;
-    imem.resize(imem_length);
+    imem = conver(immembyte);
 
-    vector<uint32_t> dmem;
+    std::cout << imem[0] << '\n';
+
+
+
+
+    std::vector<uint32_t> dmem;
     dmem.resize(dmem_length);
 
-    vector<uint32_t> reg;
+    std::vector<uint32_t> reg = {0};
     reg.resize(32);
 
     uint32_t pc = imem_off;
@@ -60,9 +74,26 @@ using namespace std;
     }
 
     return 1;
-} */
+}
 
-int main() {
+
+
+void printvector(std::vector<unsigned char> v){
+  for (int i=0; i < v.size(); i++){
+    std::cerr << +v[i] << '\n';
+  }
+}
+
+std::vector<uint32_t> void convert(std::vector<unsigned char> vec){
+  std::vector<uint32_t> final;
+  for (int i=0; i < vec.size(); i++){
+    uint32_t tmp = vec[0] | vec[1] << 8 | vec[2] << 16 | vec[3] << 32;
+    std::cout << tmp << '\n';
+    final.push_back(tmp);
+  }
+  return final;
+}
+/*int main() {
 
     cout << "Enter r j or i" << endl;
     char input;
@@ -73,4 +104,4 @@ int main() {
     cout << yeye.content() << endl;
 
     return 0;
-}
+}*/
