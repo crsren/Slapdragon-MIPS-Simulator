f944e5412580130cde0ef6b18393ab24939a7ae7
Chris <chris.renschler@gmail.com>
2019-11-07 21:45:08 +0000

    Main reads in binary file and executes associated instruction!

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -20,87 +20,90 @@ void printvector(std::vector<unsigned char> v);
 std::vector<uint32_t> convert(std::vector<unsigned char> vec);
 void printvector(std::vector<uint32_t> v);
 
-// int main(int argc, char *argv[]) {
-//
-//
-//
-//     std::cerr << "Simulating binary " << argv[1] << std::endl;
-//
-//     std::ifstream bin_stream(argv[1], std::ios::binary);
-//
-//
-//     //get length of binary instructions of input file
-//     bin_stream.seekg(0, bin_stream.end);
-//     int bin_length = bin_stream.tellg();
-//     bin_stream.seekg(0, bin_stream.beg);
-//
-//     std::vector<uint32_t> imem;
-//     imem.resize(bin_length); //taking all memory
-//
-//
-//     // temporarily store all binary instructions character by character
-//     //char * buffer = new char[bin_length];
-//     //bin_stream.read(&buffer[0], bin_length);
-//     std::cerr << bin_length << '\n';
-//     std::vector<unsigned char> immembyte(std::istreambuf_iterator<char>(bin_stream), {});
-//
-//     std::cerr << "Read " << immembyte.size() << std::endl;
-//     //printvector(immembyte);
-//     imem = convert(immembyte);
-//
-//     printvector(imem);
-//
-//     std::vector<uint32_t> dmem;
-//     dmem.resize(dmem_length);
-//
-//     std::vector<uint32_t> reg = {0};
-//     reg[0] = 32;
-//     reg[1] = 64;
-//     reg.resize(32);
-//
-//     int pc = 0;
-//
-//     while(1) {
-//
-//       //next(pc);
-//
-//       if (pc == imem.size()){
-//         std::cout << "Sucess finished" << '\n';
-//         exit(0);
-//       }
-//
-//       uint32_t word = imem[pc]; // !!!!! why / 4
-//       instruction current(word);
-//       current.showContent();
-//       memhelp::showregisters(reg);
-//
-//       //} else {
-//           //memory exception
-//         //  exit(-11);
-//       //}
-//       pc++;
-//     }
-//
-//     return 1;
-// }
+int main(int argc, char *argv[]) {
 
-int main() {
+
+
+    std::cerr << "Simulating binary " << argv[1] << std::endl;
+
+    std::ifstream bin_stream(argv[1], std::ios::binary);
+
+
+    //get length of binary instructions of input file
+    bin_stream.seekg(0, bin_stream.end);
+    int bin_length = bin_stream.tellg();
+    bin_stream.seekg(0, bin_stream.beg);
+
+    std::vector<uint32_t> imem;
+    imem.resize(bin_length); //taking all memory
+
+
+    // temporarily store all binary instructions character by character
+    //char * buffer = new char[bin_length];
+    //bin_stream.read(&buffer[0], bin_length);
+    std::cerr << bin_length << '\n';
+    std::vector<unsigned char> immembyte(std::istreambuf_iterator<char>(bin_stream), {});
+
+    std::cerr << "Read " << immembyte.size() << std::endl;
+    //printvector(immembyte);
+    imem = convert(immembyte);
+
+    printvector(imem);
+
+    std::vector<uint32_t> dmem;
+    dmem.resize(dmem_length);
+
     std::vector<uint32_t> reg = {0};
     reg.resize(32);
-    reg[1] = 1;
-    reg[2] = 2;
-    memhelp::showregisters(reg);
+
+    reg[1] = 32;
+    reg[2] = 64;
 
     uint32_t pc = 0;
-    uint32_t input = 0b10000100000001000001000001000000;
-    instruction ins;
-    ins.init(input);
-    ins.showContent();
-    ins.run(pc, reg);
-    memhelp::showregisters(reg);
 
-    return 0;
+    while(1) {
+
+      //next(pc);
+
+      if (pc == imem.size()){
+        std::cout << "Sucess finished" << '\n';
+        exit(0);
+      }
+
+      uint32_t word = imem[pc]; // !!!!! why / 4
+      instruction ins;
+      ins.init(word);
+      ins.showContent();
+      ins.run(pc, reg);
+      memhelp::showregisters(reg);
+
+      //} else {
+          //memory exception
+        //  exit(-11);
+      //}
+      pc++;
+    }
+
+    return 1;
 }
+
+// int main() {
+    // std::vector<uint32_t> reg = {0};
+    // reg.resize(32);
+    // reg[1] = 1;
+    // reg[2] = 2;
+    // memhelp::showregisters(reg);
+    //
+    // uint32_t pc = 0;
+    // uint32_t input = 0b10000100000001000001000001000000;
+    // instruction ins;
+    // ins.init(input);
+    // ins.showContent();
+    // ins.run(pc, reg);
+    // memhelp::showregisters(reg);
+//
+//     return 0;
+// }
 
 
 void printvector(std::vector<unsigned char> v){
@@ -119,7 +122,7 @@ void printvector(std::vector<uint32_t> v){
 std::vector<uint32_t> convert(std::vector<unsigned char> vec){
   std::vector<uint32_t> final;
   for (int i=0; i < vec.size(); i = i + 4){
-    uint32_t tmp = vec[i] | vec[i+1] << 8 | vec[i+2] << 16 | vec[i+3] << 24;
+    uint32_t tmp = vec[i+3] | vec[i+2] << 8 | vec[i+1] << 16 | vec[i] << 24;
     final.push_back(tmp);
   }
   return final;
