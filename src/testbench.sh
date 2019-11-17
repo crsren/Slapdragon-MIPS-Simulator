891c2aad62ef383909011224e0f37d9d84b5e63a
Chris <chris.renschler@gmail.com>
2019-11-17 17:07:02 +0000

    Testbench now calls all tests in a given directory and stores cout and return code. Started CSV pushing

--- src/testbench.sh
+++ src/testbench.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-EXIT_STATUS = $?
-TEST = $("ADDU-wrap") #$1
-SIMULATOR = $("mips_simulator")
-#later the input $1 will be the name of the simulator and the tests wil be predefined (?)
+chmod 755 ttt/*
 
-echo ./bin/$(SIMULATOR) ./testsThomas/$(TEST).mips.bin
+SIMULATOR=$1
+
+> out.csv #clean csv file
+
+for i in ttt/ADDU-wrap.mips.bin; do
+
+TEST=${i%%.*}; #remove suffix
+
+$SIMULATOR $i
+
+echo "------------------------------------------------------------"
+echo $TEST
+
+#echo $? > $TEST.got.retcode
+
+#echo $1 > $TEST.got.stdout #not sure if this will capture all stdcout
+
+cat $TEST.got.retcode" " >> out.csv
+done
