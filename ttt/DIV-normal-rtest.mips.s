70f817c6c21dda8731d6eb22a1b8589d728361fd
Nikita Belenkov <nikitov603@gmail.com>
2019-11-19 00:47:26 +0000

    finally added DIV tests duh

---
+++ ttt/DIV-normal-rtest.mips.s
@@ -0,0 +1,25 @@
+ lui     v0,0x7fff
+ ori     v0,v0,0xffff
+ lui     v1,0x7fff
+ ori     v1,v1,0xffff
+ div     $a0,v1
+ ssl     $zero, $zero, 0
+ ssl     $zero, $zero, 0
+ mfhi    v0
+
+ jr      ra
+ ssl     $zero, $zero, 0
+
+
+0x3C027FFF
+0x3442FFFF
+0x3C037FFF
+0x3463FFFF
+0x0043001A
+0x00000000
+0x00000000
+0x00001010
+0x03E00008
+0x00000000
+
+3C027FFF3442FFFF3C037FFF3463FFFF0043001A00000000000000000000101003E0000800000000
