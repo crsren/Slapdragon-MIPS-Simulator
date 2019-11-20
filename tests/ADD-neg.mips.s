bd4ab2641d209c0ed24be28279dbba290f670c5f
Chris <chris.renschler@gmail.com>
2019-11-20 16:09:47 +0000

    4 testcases for ADD

---
+++ tests/ADD-neg.mips.s
@@ -0,0 +1,13 @@
+# Created by car3418
+# Adding to a negative integer without overflow
+# Return code: 253
+
+ori $8, $0, 0x0002
+addi $9, $0, -5
+#r8 = 2 and r9 = -5
+
+add $2, $8, $9
+#r2 = -3 = unsigned 253
+
+jr $0
+
