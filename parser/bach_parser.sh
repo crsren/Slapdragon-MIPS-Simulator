bc01062de6c99156bc7767f3b289ee43997d742f
Chris <chris.renschler@gmail.com>
2019-11-22 18:09:59 +0000

    hopefully final edit to parser
    extra directory for manual binaries

--- parser/bach_parser.sh
+++ parser/bach_parser.sh
@@ -16,7 +16,8 @@ do
 
 	#so that we dont have to create extra files for retcode, stdout and stdin
 	RetCodeLine=$(grep "code" $FILE)
-	RETCODE=${RetCodeLine##*": "}
+	tmp=${RetCodeLine##*": "}
+	RETCODE=${tmp%%" "*}
 	echo $RETCODE > $DEST/$NAME.ref.retcode
 	StdoutLine=$(grep "tdout" $FILE)
 	STDOUT=${StdoutLine##*": "}
@@ -31,5 +32,5 @@ echo -e "Finished.\n"
 
 rm ../tests/J-delay.mips.bin
 rm ../tests/INTERNAL-invalid-instruction.mips.bin
-cp ../../J-delay.mips.bin ../tests/J-delay.mips.bin
-cp ../../INTERNAL-invalid-instruction.mips.bin ../tests/INTERNAL-invalid-instruction.mips.bin
+cp ../xtratests/J-delay.mips.bin ../tests/J-delay.mips.bin
+cp ../xtratests/INTERNAL-invalid-instruction.mips.bin ../tests/INTERNAL-invalid-instruction.mips.bin
