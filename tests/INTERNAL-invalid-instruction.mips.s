048b7aa5ef123e17ef639ffc996c88083416680a
Chris <chris.renschler@gmail.com>
2019-11-22 18:10:53 +0000

    Adapted manual testcases

--- tests/INTERNAL-invalid-instruction.mips.s
+++ tests/INTERNAL-invalid-instruction.mips.s
@@ -2,7 +2,5 @@
 # Checking exception handling for an invalid instruction
 # Return code: 244
 
-THIS LINE MAKES SURE THE PARSER WONT COMPILE THIS
-
-0x00000001
-(r type with invalid fn code)
+#0x00000001
+#(r type with invalid fn code)
