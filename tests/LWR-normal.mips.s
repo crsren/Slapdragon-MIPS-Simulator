f43e1b65aa6030f64b7c046d22cfa7567963e62b
Nikita Belenkov <nikitov603@gmail.com>
2019-11-22 03:47:14 +0000

    LWR and LWL

--- tests/LWR-normal.mips.s
+++ tests/LWR-normal.mips.s
@@ -1,21 +1,19 @@
 # Created by nb2618
 # normal operation test
 # Return code: 187
-# stdout: d
+# stdout: [
 
 lui $7, 0x2000
 
-lui $4, 0xDEBB
-ori $4, $4, 0x1234
-
-ori $2, $2, 100
+lui $4, 0x5BBB
+ori $4, $4, 0x7843
 
 lui $10, 0x3000
 
 sw $4,  0($7)
-lwl $2,  1($7)
-sb   $2, 7($10)
-srl $2, $2, 24
+lwr $2,  1($7)
+srl $4, $2, 8
+sb $4, 7($10)
 
 jr $0
 nop
